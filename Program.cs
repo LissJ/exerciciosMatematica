48bbf09820ac63e5063b1188820e40c281d066a3
Liss <lissj014@gmail.com>
2024-04-17 16:29:26 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exerciciosMatematica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*-----------------------------------------------------------*/
+
+            /*------------------------EXERCICIO 1------------------------*/
+
+            /*int horas = 8;
+            int salario = 35;
+
+            int salarioDiaInteiro = horas * salario;
+
+            Console.WriteLine("Exercício 1");
+            Console.WriteLine("O salário de 8 horas de trabalho de Joaquim é de R$"+ salarioDiaInteiro);*/
+
+            /*-----------------------------------------------------------*/
+
+            /*------------------------EXERCICIO 2------------------------*/
+
+            /*int quantBombom = 27;
+            int quantCaixa = 16;
+
+            int totalBombom = quantBombom * quantCaixa;
+
+            Console.WriteLine("Exercício 2");
+            Console.WriteLine("Pamela possui " + totalBombom + " bombons.");*/
+
+            /*-----------------------------------------------------------*/
+
+            /*------------------------EXERCICIO 3------------------------*/
+
+            /*int n1 = 5; 
+            int n2 = 10;
+
+            int soma = n1 + n2;
+
+            Console.WriteLine("Exercício 3");
+            Console.WriteLine("A soma do número " + n1 + " com o número " + n2 + " é igual a " + soma);*/
+
+            /*-----------------------------------------------------------*/
+
+            /*------------------------EXERCICIO 4------------------------*/
+
+            /*int garrafas = 300;
+            int caixasGarrafas = 20;
+
+            float quantCaixaUsada = garrafas / caixasGarrafas;
+
+            Console.WriteLine("Exercício 4");
+            Console.WriteLine("A loja utilizou " + quantCaixaUsada + " caixas.");*/
+
+            /*-----------------------------------------------------------*/
+
+            /*------------------------EXERCICIO 5------------------------*/
+
+            /*double instalacao = 300.00;
+            double limpeza = 150.00;
+            double manutencao = 499.99;
+
+            double somaServicos = instalacao + limpeza + manutencao;
+
+            double desconto = somaServicos - 50;
+
+            Console.WriteLine("Exercício 5");
+            Console.WriteLine("O novo cliente deverá pagar " + desconto + " reais (desconto de R$50,00).");*/
+        }
+    }
+}
